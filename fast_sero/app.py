c84349551db168368c8cbda320ae92c7b1e5d2b4
Brian_martins <brianmartins997232779@gmail.com>
2025-05-19 22:17:06 -0300

    segungo

--- fast_sero/app.py
+++ fast_sero/app.py
@@ -27,6 +27,15 @@ def create_user(user: UserSchema):
 def read_users():
     return {'users': database}
 
+@app.get('/users/{user_id}', response_model=UserPublic)
+def busca_id(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    return database[user_id - 1]
+
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
