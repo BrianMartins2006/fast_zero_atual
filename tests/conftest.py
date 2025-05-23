33e2cfc156004c5c0c78b561867636a90585a9ee
Brian_martins <brianmartins997232779@gmail.com>
2025-05-23 14:37:28 -0300

    versao atualizada

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,33 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fast_sero.app import app
-from fast_sero.models import table_registry
+from fast_sero.database import get_session
+from fast_sero.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    with TestClient(app) as client:
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thred': False},
+        pollclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
@@ -44,3 +58,14 @@ def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
