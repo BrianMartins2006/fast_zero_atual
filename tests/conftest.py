d166f4101d77b9cfc79338f09b385b05a64be8b3
Brian_martins <brianmartins997232779@gmail.com>
2025-05-25 14:25:59 -0300

    Refatorando a estrutura do projetoo

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from sqlalchemy.pool import StaticPool
 from fast_sero.app import app
 from fast_sero.database import get_session
 from fast_sero.models import User, table_registry
-from fast_sero.security import get_password_has
+from fast_sero.security import get_password_hash
 
 
 @pytest.fixture
@@ -18,9 +18,8 @@ def client(session):
     def get_session_override():
         return session
 
-    app.dependency_overrides[get_session] = get_session_override
-
     with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
         yield client
 
     app.dependency_overrides.clear()
@@ -30,8 +29,8 @@ def client(session):
 def session():
     engine = create_engine(
         'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},  # CORRETO
-        poolclass=StaticPool,  # CORRETO
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
     )
     table_registry.metadata.create_all(engine)
 
@@ -63,17 +62,25 @@ def mock_db_time():
 
 @pytest.fixture
 def user(session):
-    pwd = 'testtest'
+    password = 'testtest'
     user = User(
         username='Teste',
         email='teste@test.com',
-        password=get_password_has(pwd),
+        password=get_password_hash('testtest'),
     )
-
     session.add(user)
     session.commit()
     session.refresh(user)
 
-    user.clean_password = pwd
+    user.clean_password = password
 
     return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/auth/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    return response.json()['access_token']
